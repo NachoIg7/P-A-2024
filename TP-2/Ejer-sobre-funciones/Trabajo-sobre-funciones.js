e69b29d5f896196a155da0f16073b9429b3332ec
ignacio <nachoilg1997@gmail.com>
2024-08-27 22:26:20 -0300

    primer cambio desde cosola

---
+++ TP-2/Ejer-sobre-funciones/Trabajo-sobre-funciones.js
@@ -0,0 +1,52 @@
+// 1. Función Suma:
+// ○ Crea una función llamada sumar que tome dos números como
+// parámetros y devuelva su suma. Prueba la función con diferentes
+// números.
+function suma(a, b) {
+    return a + b;
+}
+
+console.log(suma(5, 10));
+
+
+// 2. Función que Multiplica:
+// ○ Crea una función llamada multiplicar que tome dos números como
+// parámetros y devuelva su producto. Prueba la función con diferentes
+// valores.
+
+
+
+// 3. Función con Parámetro por Defecto:
+// ○ Crea una función llamada saludar que tome un parámetro nombre
+// con un valor por defecto de "Invitado". La función debe devolver
+// "Hola, [nombre]".
+
+// 4. Función que Devuelve un Objeto:
+// ○ Crea una función llamada crearPersona que tome nombre y edad
+// como parámetros y devuelva un objeto con esas propiedades.
+
+// 5. Función que Modifica un Objeto:
+// ○ Crea una función llamada actualizarEdad que tome un objeto
+// persona y un número nuevaEdad, y actualice la propiedad edad del
+// objeto.
+// 6. Función Recursiva:
+// ○ Crea una función recursiva llamada factorial que calcule el factorial
+// de un número dado.
+// 7. Función con Función Interna:
+// ○ Crea una función llamada despedir que contenga una función
+// interna adios. despedir debe devolver el resultado de llamar a adios.
+
+// 8. Función que Usa Otra Función:
+// ○ Crea una función llamada procesarArray que tome un array y una
+// función como parámetros, y aplique la función a cada elemento del
+// array. Usa una función como parámetro para multiplicar cada
+// número por 2.
+// 9. Función que Devuelve Otra Función:
+// ○ Crea una función llamada crearMultiplicador que tome un número x
+// y devuelva una nueva función que multiplique cualquier número por
+// x.
+
+// 10. Función Anónima:
+// ○ Crea una función anónima que sume dos números y la asigna a una
+// variable llamada sumarAnonima. Luego, invoca la función a través de
+// esa variable.
